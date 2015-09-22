3929d9b0487f418496208ad9761c254aa5cbd51b
David <obrien.david@outlook.com>
2015-09-22 12:55:11 +1000

    added custom PowerShell DSC resource to install modules from PS Gallery

---
+++ PowerShellModule/PowerShellModule.psm1
@@ -0,0 +1,82 @@
+﻿enum ensure {
+    absent
+    present
+}
+
+[DscResource()]
+class PSModuleResource {
+
+    [DscProperty(Key)]
+    [string] $Module_Name
+
+    [DscProperty(Mandatory)]
+    [Ensure] $Ensure
+
+    [PSModuleResource] Get () {
+        
+        $state = [hashtable]::new()
+        $state.Module_Name = $this.Module_Name
+
+        $Module = Get-Module -Name $this.Module_Name -ListAvailable -ErrorAction Ignore
+        if ($Module) {
+            $state.Ensure = [ensure]::present
+        }
+        else {
+            $state.Ensure = [ensure]::absent
+        }
+
+        return [PSModuleResource] $state
+
+    }
+
+
+    [void] Set () {
+
+        if ($this.Ensure -eq 'present') {
+            try {
+                Find-Module -Name $this.Module_Name -ErrorAction Stop
+            }
+            catch {
+                Write-Error -ErrorRecord $_
+                throw $_
+            }
+
+            try {
+                Install-Module -Name $this.Module_Name -Force
+            }
+            catch {
+                Write-Error -ErrorRecord $_
+            }
+        }
+        elseif ($this.Ensure -eq 'absent') {
+            Uninstall-Module -Name $this.Module_Name -Force
+        }
+        else {
+            Write-Verbose -Message 'This should never be reached'
+        }
+
+    }
+
+    [bool] Test () {
+        
+        $Module = Get-Module -Name $this.Module_Name -ListAvailable -ErrorAction Ignore
+
+        if ($Module -and ($this.Ensure -eq 'present')) {
+            return [bool] $true
+        }
+        elseif ((-not $Module) -and ($this.Ensure -eq 'absent')) {
+            return [bool] $true
+        }
+        elseif (($Module) -and ($this.Ensure -eq 'absent')) {
+            return [bool] $false
+        }
+        elseif ((-not $Module) -and ($this.Ensure -eq 'present')) {
+            return [bool] $false
+        }
+        else {
+            Write-Verbose -Message 'THis should never be reached'
+            return [bool] $false
+        }
+    }
+
+}
